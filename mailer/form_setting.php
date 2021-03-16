1f95093e7ef089341e4c907e5f9aed79db1725b5
oceansf <oceansfuaga@gmail.com>
2021-03-16 13:05:08 -0700

    Update contact form

--- mailer/form_setting.php
+++ mailer/form_setting.php
@@ -1,8 +1,8 @@
 <?php
 	/*Form settings*/
-	$subj = "New message from the site 'Cvio - Resume/CV Template'"; //letter subject
-	$to = 'beshleyua@gmail.com'; // Enter Your E-mail
-	$from = 'admin@you-site-name.com'; // Admin e-mail
-	$fromName = 'Your Company Name'; // Your company name
+	$subj = "New message from the site oceanfuaga.com"; //letter subject
+	$to = 'oceansfuaga@gmail.com'; // Enter Your E-mail
+	$from = 'oceansfuaga@gmail.com'; // Admin e-mail
+	$fromName = 'Ocean Fuaga'; // Your company name
 	$charset = 'UTF-8';
 ?>
